81b619bdb3c6cb4cecab0201ee59b8313de5fac4
Md Fattan Prodan Pathul <prodanfattan@gmail.com>
2024-02-01 22:55:03 +0600

    dishesController

---
+++ server/controller/dishesControllers.js
@@ -0,0 +1,119 @@
+const dishesModel = require("../model/dishesModel.js");
+const categoryModel = require("../model/categoryModel");
+
+// create dishes
+
+const createDishes = async (req, res) => {
+  try {
+    const {
+      res_id,
+      branchID,
+      title,
+      category,
+      isActive,
+      description,
+      supplementary_duty,
+      img,
+      price,
+      preparation_cost,
+      offerPrice,
+      sales_tax,
+      options,
+      addOn,
+    } = req.body;
+    if (dishesModel.findOne({ title })) {
+      return res.status(400).send({
+        success: false,
+        message: "Dish Already Exists",
+      });
+    }
+
+    const dish = await new dishesModel({
+      res_id,
+      branchID,
+      title,
+      category,
+      isActive,
+      description,
+      supplementary_duty,
+      img,
+      price,
+      preparation_cost,
+      offerPrice,
+      sales_tax,
+      options,
+      addOn,
+    }).save();
+    res.status(200).send(true);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(false);
+  }
+};
+
+//update dishes
+const updateDish = async (req, res) => {
+  try {
+    const {
+      res_id,
+      branchID,
+      title,
+      category,
+      isActive,
+      description,
+      supplementary_duty,
+      img,
+      price,
+      preparation_cost,
+      offerPrice,
+      sales_tax,
+      options,
+      addOn,
+    } = req.body;
+    // if (!dishesModel.findOne({ title })) {
+    //   return res.status(400).send({
+    //     success: false,
+    //     message: "Dish Doesn't Exists",
+    //   });
+    // }
+    const dishThatNeedUpdate = await dishesModel.findOne({ title });
+    const dish = await dishesModel.findByIdAndUpdate(
+      dishThatNeedUpdate._id,
+      {
+        res_id,
+        branchID,
+        title,
+        category,
+        isActive,
+        description,
+        supplementary_duty,
+        img,
+        price,
+        preparation_cost,
+        offerPrice,
+        sales_tax,
+        options,
+        addOn,
+      },
+      { new: true }
+    );
+    res.status(200).send(true);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(false);
+  }
+};
+
+//delete dish
+const deleteDish = async (req, res) => {
+  try {
+    const _id = req.params._id;
+    await dishesModel.findByIdAndUpdate(
+      _id,
+      {
+        deleteStatus: true,
+      },
+      { new: true }
+    );
+  } catch (err) {}
+};
