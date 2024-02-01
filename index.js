65467a9e0fe657a32906d9ec8d0617c7d1fff286
RaaS <gazitahsin323@gmail.com>
2024-02-01 11:54:17 +0600

    cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv')
 const express = require("express");
 const app = express();
 const connectDB = require("./server/config/database");
-
+const cors = require('cors');
 
 
 
@@ -11,6 +11,20 @@ const port = process.env.PORT || 5000;
 
 
 connectDB();
+
+const corsOptions = {
+  origin: [ "*" ],
+  credentials: true,
+};
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', corsOptions.origin);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
+app.use(cors(corsOptions));
 
 
 
