d5de3a0ca07027263fd61d729395e80eeef4da31
Md Fattan Prodan Pathul <prodanfattan@gmail.com>
2024-02-05 15:55:03 +0600

    subcription controller

---
+++ server/controller/subscriptionController.js
@@ -0,0 +1,73 @@
+const Subscription = require("../models/subscription");
+
+// Create a new subscription
+const createSubscription = async (req, res) => {
+  try {
+    const { res_id, packageType } = req.body;
+    let startDate = Date.now();
+    let endDate;
+    switch (packageType) {
+      case "3 month":
+        endDate = startDate + 3 * 30 * 24 * 60 * 60 * 1000;
+        break;
+      case "6 month":
+        endDate = startDate + 6 * 30 * 24 * 60 * 60 * 1000;
+        break;
+      case "12 month":
+        endDate = startDate + 12 * 30 * 24 * 60 * 60 * 1000;
+        break;
+      default:
+        return res.status(400).json({ error: "Invalid package type" });
+    }
+
+    const newSubscription = new Subscription({
+      res_id,
+      packageType,
+      startDate,
+      endDate,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    }).save();
+    res.status(201).send(true);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+//when user expend their subscription
+const extendSubscription = async (req, res) => {
+  try {
+    const { subscriptionId, packageType } = req.body;
+
+    const existingSubscription = await Subscription.findById(subscriptionId);
+    if (!existingSubscription) {
+      return res.status(404).json({ error: "Subscription not found" });
+    }
+    let newEndDate;
+    switch (packageType) {
+      case "3 month":
+        newEndDate =
+          existingSubscription.endDate + 3 * 30 * 24 * 60 * 60 * 1000;
+        break;
+      case "6 month":
+        newEndDate =
+          existingSubscription.endDate + 6 * 30 * 24 * 60 * 60 * 1000;
+        break;
+      case "12 month":
+        newEndDate =
+          existingSubscription.endDate + 12 * 30 * 24 * 60 * 60 * 1000;
+        break;
+      default:
+        return res.status(400).json({ error: "Invalid package type" });
+    }
+    existingSubscription.endDate = newEndDate;
+    existingSubscription.packageType = packageType;
+    existingSubscription.updatedAt = Date.now();
+    await existingSubscription.save();
+    res.status(200).send(true);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
