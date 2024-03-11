2d8f503a88697a9e5019ee3f833dea12d268c636
Md Fattan Prodan Pathul <prodanfattan@gmail.com>
2024-03-11 16:30:51 +0600

    dev

--- server/controller/devSubscriptionsEdit.js
+++ server/controller/devSubscriptionsEdit.js
@@ -1,3 +1,21 @@
 const Subscription = require("../model/subscriptionModel");
 const sendMail = require("../utils/sendEmail.js");
 const { getAllBranch } = require("../controller/branchController.js");
+
+const deleteBranchFromDevPaymentList = async (req, res) => {
+  try {
+    const _id = req.params._id;
+    await Subscription.findByIdAndUpdate(
+      _id,
+      {
+        deleteStatus: true,
+      },
+      { new: true }
+    );
+    res.send(getAllBranch()); //return updated branch list after deleting the selected one from dev payment list
+  } catch (err) {}
+};
+
+module.exports = {
+  deleteBranchFromDevPaymentList,
+};
