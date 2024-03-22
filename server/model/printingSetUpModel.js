5dcc90ce078e6fd0f437d54ca05920d5e68d8296
RaaS <gazitahsin323@gmail.com>
2024-03-22 17:41:13 +0600

    printing set up controller modified

---
+++ server/model/printingSetUpModel.js
@@ -0,0 +1,40 @@
+const mongoose = require( 'mongoose' );
+
+const printingSetUpSchema = new mongoose.Schema({
+    branchID:{
+        type: mongoose.ObjectId,
+        ref: "Branchs",
+    },
+    headerText:{
+        type:String,
+
+    },
+    greetingText:{
+        type:String,
+    },
+    print_address:{
+        type:Boolean,
+        default:false
+    },
+    print_logo:{
+        type:Boolean,
+        default:false
+    },
+    print_res_email:{
+        type:Boolean,
+        default:false
+    },
+    print_res_mobile:{
+        type:Boolean,
+        default:false
+    },
+    print_kitchen_print:{
+        type:Boolean,
+        default:false
+    },
+
+},
+{ timestamps: true });
+module.exports = mongoose.model("printingSetUp", printingSetUpSchema);
+
+
