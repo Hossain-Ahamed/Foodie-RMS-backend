950fab58654fbabf3b90c25c2d21c8efd8c0cf61
Md Fattan Prodan Pathul <prodanfattan@gmail.com>
2024-03-23 17:08:20 +0600

    dishes by id, printingSetup model, tableModel

--- server/model/tableModel.js
+++ server/model/tableModel.js
@@ -1,29 +1,29 @@
-const mongoose = require ('mongoose');
+const mongoose = require("mongoose");
 
-const tableSchema =  new mongoose.Schema({
-    res_id: {
-        type: mongoose.ObjectId,
-        ref: "Restaurants",
-        required: true,
-      },
-      branchID: {
-      type: mongoose.ObjectId,
-      ref: "Branchs",
-      required: true,
-      },
-    number: { 
-        type : String , 
-        required : true 
-    },
-    capacity :{
-        type : Number,
-        required:true
-    },
-    status:{
-        type:String,
-        enum:["Occupied","Vacant"],
-        default:"Vacant"
-    }
+const tableSchema = new mongoose.Schema({
+  res_id: {
+    type: mongoose.ObjectId,
+    ref: "Restaurants",
+    required: true,
+  },
+  branchID: {
+    type: mongoose.ObjectId,
+    ref: "Branchs",
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
+  },
+  capacity: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["Occupied", "Vacant"],
+    default: "Vacant",
+  },
 });
 
-module.exports =mongoose.model("Tables",tableSchema);
+module.exports = mongoose.model("Tables", tableSchema);
