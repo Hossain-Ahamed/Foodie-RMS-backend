3237ba2921cdbc313165f1dea73845ecd24e89a1
Md Fattan Prodan Pathul <prodanfattan@gmail.com>
2024-03-25 12:36:00 +0600

    vendor by branch id

--- server/controller/vendorController.js
+++ server/controller/vendorController.js
@@ -1,9 +1,9 @@
 const vendor = require("../model/vendorModel");
-const createCustomer = async (req, res) => {
+const createVendor = async (req, res) => {
   try {
     const { name, phone, address } = req.body;
     const { res_id, branchID } = req.params;
-    await Customer({
+    await vendor({
       res_id,
       branchID,
       name,
@@ -11,6 +11,16 @@ const createCustomer = async (req, res) => {
       address,
     }).save();
     res.status(201).send(true);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+const getAllVendors = async (req, res) => {
+  try {
+    const { branchID } = req.params;
+    const vendors = await vendor.find({ branchID: branchID });
+    res.status(200).json(vendors);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
   }
