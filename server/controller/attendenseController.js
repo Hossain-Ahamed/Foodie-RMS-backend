5d3790977bbf004dd8877fd20301e170e872e0cb
Md Fattan Prodan Pathul <prodanfattan@gmail.com>
2024-04-14 12:19:31 +0600

    attendense

---
+++ server/controller/attendenseController.js
@@ -0,0 +1,41 @@
+const Attendense = require("../model/attendenseModel");
+
+const getMonthAbbreviation = (monthIndex) => {
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  return monthNames[monthIndex];
+};
+const takeAttendense = async (req, res) => {
+  try {
+    const { res_id, branchID } = req.params;
+    const { attendence } = req.body;
+    const today = new Date();
+    const date = today.toISOString().split("T")[0];
+    const monthAbbreviation = getMonthAbbreviation(today.getMonth());
+    const newAttendance = new Attendense({
+      res_id,
+      branchID,
+      date,
+      month: monthAbbreviation,
+      attendence,
+    }).save();
+    res.status(201).json(newAttendance);
+  } catch (error) {
+    res.status(500).json({ error: "Could not create attendance" });
+  }
+};
+module.exports = {
+  takeAttendense,
+};
